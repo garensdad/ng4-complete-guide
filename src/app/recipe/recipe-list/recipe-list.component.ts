d7c1252333f242fa133795c6f7646a31078b99bc
garensdad <garensdad@gmail.com>
2017-09-04 08:50:37 -0400

    Some lists added

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -8,7 +8,10 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is a test Recipe', 'https://get.pxhere.com/photo/dish-food-recipe-eat-meat-cuisine-steak-veal-fillet-venison-red-meat-beef-tenderloin-rib-eye-steak-roast-beef-animal-source-foods-kobe-beef-sirloin-steak-corned-beef-lamb-and-mutton-fillet-of-heidschnucke-carne-asada-762027.jpg')
+  ];
+
   constructor() { }
 
   ngOnInit() {
